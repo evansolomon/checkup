0569bd1a25db7f0ad854484b5ece2c2a02fb7cc8
Steve Calvert <scalvert@linkedin.com>
2021-02-18 17:14:08 -0800

    chore: Adding low-level SARIF result builder

--- packages/core/src/data/sarif.ts
+++ packages/core/src/data/sarif.ts
@@ -6,6 +6,24 @@ export const NO_RESULTS_FOUND = 'No results found';
 type SarifTaskIdentifier = Pick<Task, 'taskName' | 'taskDisplayName' | 'category' | 'group'>;
 
 /**
+ *
+ * @param ruleId The SARIF Result.ruleId property.
+ * @param message The SARIF Result.message.text property.
+ * @param rest Additional properties to populate a SARIF Result.
+ */
+function buildResult(
+  ruleId: string,
+  message: string,
+  rest: Omit<Result, 'ruleId' | 'message'>
+): Result {
+  return {
+    ruleId,
+    message: { text: message },
+    ...rest,
+  };
+}
+
+/**
  * Builds SARIF Results from a list of LintResults.
  *
  * @param taskContext {Task} This is used to set Task properties on the Result
@@ -126,6 +144,7 @@ export function fromTaskErrors(errors: TaskError[]): Notification[] {
 }
 
 export const sarifBuilder = {
+  buildResult,
   fromLintResults,
   fromLocations,
   fromData,
